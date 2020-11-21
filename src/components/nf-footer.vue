c235f8a01d9e35739926b44df41a359eefb54f59
Natural2015 <2044816986@qq.com>
2020-11-21 21:57:12 +0800

    1

---
+++ src/components/nf-footer.vue
@@ -0,0 +1,9 @@
+<template>
+	<div><span><h1>This a footer</h1></span></div>
+</template>
+
+<script>
+</script>
+	export default;
+<style>
+</style>
---
+++ src/components/nf-header.vue
@@ -0,0 +1,18 @@
+<template>
+	<div><span>
+			<h1>This a header</h1>
+		</span></div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {}
+		}
+	}
+</script>
+<style scoped>
+	span {
+		color: aquamarine;
+	}
+</style>
